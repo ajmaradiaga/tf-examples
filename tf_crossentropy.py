0891a78a61d0494cb9ff13b1b3a7294ec0f62a29
Antonio Maradiaga <me@ajmaradiaga.com>
2017-04-13 10:46:32 +0100

    Initial Commit
    Adding first batch of TensorFlow examples: Install Test, Linear Function, Math functions, Softmax, Cross Entropy

---
+++ tf_crossentropy.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+softmax_data = [0.7, 0.2, 0.1] #yhat
+one_hot_data = [1.0, 0.0, 0.0] #y
+
+softmax = tf.placeholder(tf.float32)
+one_hot = tf.placeholder(tf.float32)
+
+# Print cross entropy from session
+# y*ln(yhat)
+ce = tf.reduce_sum(tf.multiply(one_hot, tf.log(softmax)))
+
+output = None
+
+with tf.Session() as sess:
+    output = sess.run(ce, feed_dict={one_hot: one_hot_data, softmax:softmax_data})
+
+print("Cross Entropy of y: {} and yhat: {} is {}".format(one_hot_data, softmax_data, output))
